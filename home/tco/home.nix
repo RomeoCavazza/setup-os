3583d1e7673f663504d573e82a47774b741ba840
RomeoCavazza <romeo.cavazza@gmail.com>
2026-03-11 17:19:30 +0100

    config: edex-toggle, hyprland.conf, home.nix

--- home/tco/home.nix
+++ home/tco/home.nix
@@ -1,6 +1,27 @@
 { config, pkgs, lib, inputs, ... }:
 let
   mkOut = config.lib.file.mkOutOfStoreSymlink;
+
+  terminal-rain-lightning = pkgs.python3Packages.buildPythonApplication rec {
+    pname = "terminal-rain-lightning";
+    version = "master";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "rmaake1";
+      repo = "terminal-rain-lightning";
+      rev = "master";
+      hash = "sha256-ghMqdEff2VLisCBG+GMZBxw7Ka7Y6KjLsDxwnm1njOQ=";
+    };
+
+    format = "pyproject";
+
+    nativeBuildInputs = with pkgs.python3Packages; [
+      setuptools
+      wheel
+    ];
+
+    doCheck = false;
+  };
 in
 {
   home.username = "tco";
@@ -156,6 +177,7 @@ in
     hollywood
     pipes
     sl
+    terminal-rain-lightning
   ];
 
   gtk = {
