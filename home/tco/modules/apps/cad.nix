a1d36a55d763f304bce815ffb14bfe50d7bbc8e7
RomeoCavazza <romeo.cavazza@gmail.com>
2026-05-29 03:01:13 +0200

    feat(cad): add PlantUML, Graphviz and JDK

--- home/tco/modules/apps/cad.nix
+++ home/tco/modules/apps/cad.nix
@@ -8,6 +8,9 @@
     obsidian # Note taking / Second Brain
     kicad    # PCB & Schematic Design
     freecad  # Parametric 3D CAD
+    plantuml # Flowcharts & Diagrams
+    graphviz # Dot engine for PlantUML
+    jdk      # Java runtime for PlantUML
     # openscad
     # blender
   ];
