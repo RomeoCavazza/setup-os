6106834bc14affde95dcd6cb8ef9a2628ed39784
RomeoCavazza <romeo.cavazza@gmail.com>
2026-06-30 22:16:40 +0200

    fix(rofi): remove rectangular layer blur

--- home/tco/hyprland/config.nix
+++ home/tco/hyprland/config.nix
@@ -9,23 +9,10 @@
 let
   colors = import ../../../lib/colors.nix { inherit lib; };
   hyprConfig = pkgs.runCommand "hypr-config" { } ''
-        cp -R ${inputs.hypr-config} $out
-        chmod -R u+w $out
-        cp ${pkgs.writeText "tokens.conf" (colors.hyprland palette)} $out/conf/tokens.conf
-        cat >> $out/theme/rules.conf <<'EOF'
-
-    # --- Rofi Push ---
-    windowrule = border_color rgba(00000000), match:class ^(rofi)$
-
-    windowrule {
-        name = rofi-push-effects
-        match:class = ^(rofi)$
-
-        no_blur = true
-        no_shadow = true
-        rounding = 64
-    }
-    EOF
+    cp -R ${inputs.hypr-config} $out
+    chmod -R u+w $out
+    cp ${pkgs.writeText "tokens.conf" (colors.hyprland palette)} $out/conf/tokens.conf
+    sed -i '/^layerrule = blur on, match:namespace \^(rofi)\$/d' $out/theme/rules.conf
   '';
 in
 {
