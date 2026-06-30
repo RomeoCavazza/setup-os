81be1f60443e6bc5899a1069d5cf990c6ebd1f2a
RomeoCavazza <romeo.cavazza@gmail.com>
2026-06-30 22:21:16 +0200

    fix(rofi): drop stale layer rule

--- home/tco/hyprland/config.nix
+++ home/tco/hyprland/config.nix
@@ -12,7 +12,7 @@ let
     cp -R ${inputs.hypr-config} $out
     chmod -R u+w $out
     cp ${pkgs.writeText "tokens.conf" (colors.hyprland palette)} $out/conf/tokens.conf
-    sed -i '/^layerrule = blur on, match:namespace \^(rofi)\$/d' $out/theme/rules.conf
+    sed -i '/^layerrule = .*match:namespace \^(rofi)\$/d' $out/theme/rules.conf
   '';
 in
 {
