76d33fa0f73205ced6d027955dd0204793793200
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-01 23:06:55 +0200

    refactor(build): make build bind mount configurable

--- modules/core/build-sandbox.nix
+++ modules/core/build-sandbox.nix
@@ -1,19 +1,34 @@
-_:
-
 {
-  # --- Build Sandbox ---
-  systemd.tmpfiles.rules = [
-    "d /home/nix-build 2775 root nixbld - -"
-  ];
+  config,
+  lib,
+  ...
+}:
 
-  fileSystems."/build" = {
-    device = "/home/nix-build";
-    fsType = "none";
-    options = [
-      "bind"
-      "x-systemd.requires-mounts-for=/home"
-      "x-systemd.mkdir"
+let
+  cfg = config.local.buildSandbox.bindMount;
+in
+{
+  options.local.buildSandbox.bindMount.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Bind /home/nix-build to /build for hosts without a dedicated /build filesystem.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # --- Build Sandbox ---
+    systemd.tmpfiles.rules = [
+      "d /home/nix-build 2775 root nixbld - -"
     ];
-    neededForBoot = false;
+
+    fileSystems."/build" = {
+      device = "/home/nix-build";
+      fsType = "none";
+      options = [
+        "bind"
+        "x-systemd.requires-mounts-for=/home"
+        "x-systemd.mkdir"
+      ];
+      neededForBoot = false;
+    };
   };
 }
