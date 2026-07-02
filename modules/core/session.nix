a7fdca80c96d253163293a91f728804378c82452
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-02 17:20:00 +0200

    fix(shutdown): lazily unmount separate nix filesystems

--- modules/core/session.nix
+++ modules/core/session.nix
@@ -1,6 +1,20 @@
-_:
+{ pkgs, ... }:
+
+let
+  lazyUnmount = ''
+    [Mount]
+    LazyUnmount=yes
+  '';
+  nixLazyUnmountDropins = pkgs.runCommand "nix-lazy-unmount-dropins" { } ''
+    install -D -m 0444 ${pkgs.writeText "lazy-unmount.conf" lazyUnmount} \
+      "$out/lib/systemd/system/nix.mount.d/lazy-unmount.conf"
+    install -D -m 0444 ${pkgs.writeText "lazy-unmount.conf" lazyUnmount} \
+      "$out/lib/systemd/system/nix-store.mount.d/lazy-unmount.conf"
+  '';
+in
 
 {
   services.logind.settings.Login.KillUserProcesses = true;
   systemd.settings.Manager.DefaultTimeoutStopSec = "15s";
+  systemd.packages = [ nixLazyUnmountDropins ];
 }
