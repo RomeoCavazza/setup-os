1d04b46ff4adc34567ef7f12eacff80114aea347
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-02 17:27:52 +0200

    fix(shutdown): lazily unmount late runtime mounts

--- modules/core/session.nix
+++ modules/core/session.nix
@@ -1,20 +1,29 @@
 { pkgs, ... }:
 
 let
+  lazyUnmountUnits = [
+    "nix.mount"
+    "nix-store.mount"
+    "run-keys.mount"
+    "run-secrets.d.mount"
+    "run-wrappers.mount"
+  ];
   lazyUnmount = ''
     [Mount]
     LazyUnmount=yes
   '';
-  nixLazyUnmountDropins = pkgs.runCommand "nix-lazy-unmount-dropins" { } ''
-    install -D -m 0444 ${pkgs.writeText "lazy-unmount.conf" lazyUnmount} \
-      "$out/lib/systemd/system/nix.mount.d/lazy-unmount.conf"
-    install -D -m 0444 ${pkgs.writeText "lazy-unmount.conf" lazyUnmount} \
-      "$out/lib/systemd/system/nix-store.mount.d/lazy-unmount.conf"
-  '';
+  lazyUnmountDropins = pkgs.runCommand "lazy-unmount-dropins" { } (
+    builtins.concatStringsSep "\n" (
+      map (unit: ''
+        install -D -m 0444 ${pkgs.writeText "lazy-unmount.conf" lazyUnmount} \
+          "$out/lib/systemd/system/${unit}.d/lazy-unmount.conf"
+      '') lazyUnmountUnits
+    )
+  );
 in
 
 {
   services.logind.settings.Login.KillUserProcesses = true;
   systemd.settings.Manager.DefaultTimeoutStopSec = "15s";
-  systemd.packages = [ nixLazyUnmountDropins ];
+  systemd.packages = [ lazyUnmountDropins ];
 }
