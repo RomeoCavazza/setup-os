f1c4d09623b8c627ddb1f97fe5c8ce2675dc9a19
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-02 18:00:40 +0200

    fix(shutdown): keep nix mounted for shutdown ramfs

--- modules/core/session.nix
+++ modules/core/session.nix
@@ -1,33 +1,23 @@
 { pkgs, ... }:
 
 let
-  nixUnmountUnits = [
+  lateShutdownMountUnits = [
     "nix.mount"
     "nix-store.mount"
-  ];
-  lateRuntimeMountUnits = [
     "run-keys.mount"
     "run-secrets.d.mount"
     "run-wrappers.mount"
   ];
-  lazyUnmount = ''
-    [Mount]
-    LazyUnmount=yes
-  '';
-  lateRuntimeMount = ''
+  lateShutdownMount = ''
     [Unit]
     DefaultDependencies=no
   '';
   shutdownMountDropins = pkgs.runCommand "shutdown-mount-dropins" { } (
     builtins.concatStringsSep "\n" (
-      (map (unit: ''
-        install -D -m 0444 ${pkgs.writeText "lazy-unmount.conf" lazyUnmount} \
-          "$out/lib/systemd/system/${unit}.d/lazy-unmount.conf"
-      '') nixUnmountUnits)
-      ++ (map (unit: ''
-        install -D -m 0444 ${pkgs.writeText "runtime-mount.conf" lateRuntimeMount} \
-          "$out/lib/systemd/system/${unit}.d/runtime-mount.conf"
-      '') lateRuntimeMountUnits)
+      map (unit: ''
+        install -D -m 0444 ${pkgs.writeText "late-shutdown-mount.conf" lateShutdownMount} \
+          "$out/lib/systemd/system/${unit}.d/late-shutdown-mount.conf"
+      '') lateShutdownMountUnits
     )
   );
 in
